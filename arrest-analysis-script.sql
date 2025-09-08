109852321b7d6f5a2218f06d3a76d388fd3331f4
jonaslee17 <uhz7mq@virginia.edu>
2025-09-08 02:39:04 -0400

    added query to compare crimes by jurisdiction

--- arrest-analysis-script.sql
+++ arrest-analysis-script.sql
@@ -6,3 +6,23 @@ HAVING COUNT(*) > 1
 ORDER BY COUNT(*) DESC
 LIMIT 50;
 
+
+--comparing crimes by jurisdiction
+SELECT
+    CASE
+        WHEN "ori" = 'VA1020000' THEN 'CPD'
+        WHEN "ori" = 'VA0020300' THEN 'ACPD'
+        WHEN "ori" = 'VA0020100' THEN 'UPD'
+        ELSE "ori"
+    END AS Jurisdiction,
+    "description" AS "crime type",
+    COUNT(*) AS "arrest num"
+FROM
+    "arrests"
+GROUP BY
+    Jurisdiction,
+    "crime type"
+HAVING COUNT(*) > 50
+ORDER BY
+    Jurisdiction,
+    "arrest num" DESC;
